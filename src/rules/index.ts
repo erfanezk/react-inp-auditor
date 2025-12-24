e86673bc6e2e2eaeb1ce24de586bfa8c4c9ad2da
erfanezk <erfan.ezkat@gmail.com>
2025-12-24 08:32:45 -0800

    refactor: update import for inpCallbackYield rule to inpIncorrectYielding for improved clarity in performance analysis

--- src/rules/index.ts
+++ src/rules/index.ts
@@ -2,12 +2,12 @@ import type { Rule } from "@/types";
 import { inpAnimationCompositingRule } from "./inp-animation-compositing";
 import { inpAnimationTypeRule } from "./inp-animation-type";
 import { inpAsyncBatchProcessingRule } from "./inp-async-batch-processing";
-import { inpCallbackYieldRule } from "./inp-callback-yield";
 import { inpDomSizeRule } from "./inp-dom-size";
 import { inpEventHandlerApiCallsRule } from "./inp-event-handler-api-calls";
 import { inpEventHandlerDomManipulationRule } from "./inp-event-handler-dom-manipulation";
 import { inpEventHandlerStateUpdatesRule } from "./inp-event-handler-state-updates";
 import { inpHeavyComputationRule } from "./inp-heavy-computation";
+import { inpCallbackYieldRule } from "./inp-incorrect-yielding";
 import { inpLayoutThrashingRule } from "./inp-layout-thrashing";
 import { inpLongLoopRule } from "./inp-long-loop";
 
